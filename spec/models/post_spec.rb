a3d49b6df2866ab08f554ce575264a8aec43181f
Jxck <block.rxckin.beats@gmail.com>
2010-10-30 16:20:07 +0900

    count_words red

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -35,7 +35,7 @@ describe Post,"count_words が呼ばれた時" do
   before(:each) do
   end
 
-  it "count_words が期待した値を返す" do
+  it "count_words が rails=4, ruby=3, on=2 を返す" do
     post = posts(:short_content)
     actual = post.count_words()
     expected = Array(
@@ -43,9 +43,28 @@ describe Post,"count_words が呼ばれた時" do
                      "ruby" => 3,
                      "on" => 2
                      )
-    actual.should == actual
-
+    actual.should == expected
   end
+
+  it "count_words が hoge=20 を返す" do
+    post = posts(:hoge_20)
+    actual = post.count_words()
+    expected = Array(
+                     "hoge" => 20
+                     )
+    actual.should == expected
+  end
+
+  it "count_words が hoge=15, fuga=5 を返す" do
+    post = posts(:hoge_fuga)
+    actual = post.count_words()
+    expected = Array(
+                     "hoge" => 15,
+                     "fuga" => 5
+                     )
+    actual.should == expected
+  end
+
 
 end
 
