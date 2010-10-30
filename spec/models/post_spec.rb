b7accdc2afe1f8d211b846f3f347ddd3a94323c5
Jxck <block.rxckin.beats@gmail.com>
2010-10-30 16:06:11 +0900

    count_words red

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -28,3 +28,24 @@ describe Post do
   end
 
 end
+
+describe Post,"count_words が呼ばれた時" do
+  fixtures :posts
+
+  before(:each) do
+  end
+
+  it "count_words が期待した値を返す" do
+    post = posts(:short_content)
+    actual = post.count_words()
+    expected = Array(
+                     "rails" => 4,
+                     "ruby" => 3,
+                     "on" => 2
+                     )
+    actual.should == actual
+
+  end
+
+end
+
