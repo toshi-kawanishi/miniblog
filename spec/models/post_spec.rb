d99af5d03f850f7c88a0258cc05c419a4cb2b910
Jxck <block.rxckin.beats@gmail.com>
2010-10-30 14:25:01 +0900

    add validation and tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,29 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @post = Post.new
+  end
+
+  it "バリデーションに失敗すること" do
+    @post.should_not be_valid
+  end
+
+  it "バリデーションに成功すること" do
+    @post.title = "test"
+    @post.content = "This is a test post."
+    @post.should be_valid
+  end
+
+  it "タイトルだけ入れてもバリデーションに失敗すること" do
+    @post.title = "test"
+    @post.should_not be_valid
+  end
+
+  it "本文だけ入れてもバリデーションに失敗すること" do
+    @post.content = "This is a test post."
+    @post.should_not be_valid
+  end
+
 end
