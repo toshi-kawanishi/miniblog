c2a16b3131a65411032e699173b90f5fca94a99d
Jxck <block.rxckin.beats@gmail.com>
2010-10-30 13:12:59 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miniblog::Application.config.secret_token = '58b4178b9c38921f0b3ab9b31097e7ddb2e3cec0ac2972998e9e1b43ccf0bbac3244a045d385ae4c77d04e6d411f869ec88b0c4231ac385b9fe1d1140a1f8481'
