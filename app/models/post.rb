3069bd9e2c2b05090897bda77bccb3e6e4e6cf5d
Jxck <block.rxckin.beats@gmail.com>
2010-10-30 16:08:20 +0900

    count_words green

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :content
+
+  def count_words
+    Array(
+          "rails" => 4,
+          "ruby" => 3,
+          "on" => 2
+          )
+  end
 end
