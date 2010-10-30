e7cc1c4f421954f6349f16ff7c5a9cb6dc4e716e
Jxck <block.rxckin.beats@gmail.com>
2010-10-30 14:40:16 +0900

    refuctoring fixtures and tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,28 +2,29 @@
 require 'spec_helper'
 
 describe Post do
+  fixtures :posts
+
   before(:each) do
-    @post = Post.new
   end
 
   it "バリデーションに失敗すること" do
-    @post.should_not be_valid
+    post = posts(:empty)
+    post.should_not be_valid
   end
 
   it "バリデーションに成功すること" do
-    @post.title = "test"
-    @post.content = "This is a test post."
-    @post.should be_valid
+    post = posts(:full)
+    post.should be_valid
   end
 
   it "タイトルだけ入れてもバリデーションに失敗すること" do
-    @post.title = "test"
-    @post.should_not be_valid
+    post = posts(:title_only)
+    post.should_not be_valid
   end
 
   it "本文だけ入れてもバリデーションに失敗すること" do
-    @post.content = "This is a test post."
-    @post.should_not be_valid
+    post = posts(:content_only)
+    post.should_not be_valid
   end
 
 end
